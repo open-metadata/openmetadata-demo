222835ce5198d790acdd0bae356440890f787675
Pere Miquel Brull <peremiquelbrull@gmail.com>
2022-09-16 10:23:35 +0200

    Create redpanda schemas

--- redpanda/script.sh
+++ redpanda/script.sh
@@ -3,7 +3,10 @@ apt-get update
 apt-get install -y python3 pip curl
 curl -1sLf 'https://packages.vectorized.io/nzc4ZYQK3WRGd9sy/redpanda/cfg/setup/bash.deb.sh' | bash && apt install redpanda -y
 rpk version
-curl -s -X POST "http://redpanda:8081/subjects/create-user-request-value/versions" -H "Content-Type: application/vnd.schemaregistry.v1+json" -d '{"schema": "{\"type\":\"record\",\"name\":\"user\",\"fields\":[{\"name\":\"firstName\",\"type\":\"string\"},{\"name\":\"lastName\",\"type\":\"string\"},{\"name\":\"email\",\"type\":\"string\"}]}" }'
+curl -s -X POST "http://redpanda:8081/subjects/create-user-request-0-value/versions" -H "Content-Type: application/vnd.schemaregistry.v1+json" -d '{"schema": "{\"type\":\"record\",\"name\":\"user\",\"fields\":[{\"name\":\"firstName\",\"type\":\"string\"},{\"name\":\"lastName\",\"type\":\"string\"},{\"name\":\"email\",\"type\":\"string\"}]}" }'
+curl -s -X POST "http://redpanda:8081/subjects/create-user-request-1-value/versions" -H "Content-Type: application/vnd.schemaregistry.v1+json" -d '{"schema": "{\"type\":\"record\",\"name\":\"user\",\"fields\":[{\"name\":\"firstName\",\"type\":\"string\"},{\"name\":\"lastName\",\"type\":\"string\"},{\"name\":\"email\",\"type\":\"string\"}]}" }'
+curl -s -X POST "http://redpanda:8081/subjects/create-user-request-2-value/versions" -H "Content-Type: application/vnd.schemaregistry.v1+json" -d '{"schema": "{\"type\":\"record\",\"name\":\"user\",\"fields\":[{\"name\":\"firstName\",\"type\":\"string\"},{\"name\":\"lastName\",\"type\":\"string\"},{\"name\":\"email\",\"type\":\"string\"}]}" }'
+curl -s -X POST "http://redpanda:8081/subjects/create-user-request-3-value/versions" -H "Content-Type: application/vnd.schemaregistry.v1+json" -d '{"schema": "{\"type\":\"record\",\"name\":\"user\",\"fields\":[{\"name\":\"firstName\",\"type\":\"string\"},{\"name\":\"lastName\",\"type\":\"string\"},{\"name\":\"email\",\"type\":\"string\"}]}" }'
 echo ""
 echo "Added schema:"
 curl -s -X GET "http://redpanda:8081/schemas/ids/1"
